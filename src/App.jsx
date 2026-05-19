50b603454f6f68d37ebd05cf9dec290651238a46
Usmankh4 <usmankhanx3@hotmail.com>
2026-05-18 22:58:02 -0400

    added handleToggle function

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
 import { getCurrentWeather } from "./api/weatherApi";
 import { iconUrl} from "./utils/urlBuilder";
-
+import { celsiusToFahrenheit } from "./utils/celsiusConverter";
 function App() {
+
+
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [currentWeather, setCurrentWeather] = useState(null);
+  const [unit, setUnit] = useState("C");
   
   
 
@@ -25,6 +28,11 @@ function App() {
     }
   }
 
+  function handleToggle(){
+    setUnit(prev => prev === "C" ? "F": "C");
+  }
+  
+
   
 
   let content;
@@ -41,11 +49,13 @@ function App() {
     const iconCode = currentWeather.weather[0].icon;
     const iconUrlImage = iconUrl(iconCode)
     const description = currentWeather.weather[0].description;
+    const celsiusTemperature = currentWeather.main.temp;
+    const displayTemperature = unit === 'F' ? celsiusToFahrenheit(celsiusTemperature) : celsiusTemperature;
 
     content = (
       <div>
         <p>City: {currentWeather.name}</p>
-        <p>Temperature: {Math.round(currentWeather.main.temp)}°C</p>
+        <p>Temperature: {Math.round(displayTemperature)}°{unit}</p>
         <img src={iconUrlImage} alt={description}/>
         <p>Humidity: {currentWeather.main.humidity}%</p>
         <p> Wind: {currentWeather.wind.speed} m/s</p>
@@ -60,6 +70,15 @@ function App() {
     <div className="app">
       <Header title="Weather Dashboard" />
       <SearchForm onSearch={handleSearch} />
+      
+
+        {currentWeather && !isLoading && !error && (
+          <button onClick={handleToggle}>
+             Show °{unit === 'C' ? 'F': 'C'}
+             </button>
+        )}
+
+       
 
       {content}
     </div>
